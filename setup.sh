d8abf56aced1dd21a76f79905a76e85d31d0dede
imanefarah <71851777+imanefarah@users.noreply.github.com>
2021-07-04 02:29:10 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@ echo "[general]
 email = \"imane6farah@gmail.com\""  > ~/.streamlit/credentials.toml
 echo "[server]
 headless = true
+PORT = process.env.PORT || 5000
 port = $PORT
+
 enableCORS = true"  >> ~/.streamlit/config.toml
